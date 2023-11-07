6f7c7ae1f8735e2d239bd1d4706cefb3fb46d611
Ruby Hu <60735962+HuWenShin@users.noreply.github.com>
2023-11-06 20:22:14 -0800

    Restructure code and divide into functions
    Restructured code into functions for reusability and ease of debugging.
    Divided calculator screen into upper and lower screen for splitting the
    display of current typing number and ongoing operation. Added the append
    point function to append point and refactured the display feature into a
    function.

--- script.js
+++ script.js
@@ -1,37 +1,116 @@
-const add = function(a, b) {
+const first = "";
+const second = "";
+const operator = ""; //should i set this to null?
+const shouldResetScreen = false;
+
+const opScreen = document.getElementById("upper-screen");
+const curScreen = document.getElementById("lower-screen");
+const clrBtn = document.getElementById("clr");
+const delBtn = document.getElementById("delete");
+const ptBtn = document.getElementById("point");
+
+clrBtn.addEventListener("click", clr);
+delBtn.addEventListener("click", del);
+ptBtn.addEventListener("click", appendPoint);
+
+/*
+
+
+const equalBtn = document.getElementById("equal");
+
+const numBtn = document.querySelectorAll("[data-number]");
+const opBtn = document.querySelectorAll("[data-operator]");
+
+
+
+
+
+equalBtn.addEventListener("click", operate);
+
+
+numBtn.forEach(button => button.addEventListener("click", () => appendNumber(this.value)));
+opBtn.forEach(button =>button.addEventListener("click", () => setOperation()));
+
+function appendNumber(num) {
+    if (curScreen.textContent === "0" || shouldResetScreen === true) resetScreen();
+    curScreen.textContent += num;
+}
+
+function resetScreen() {
+    curScreen.textContent = "";
+    shouldResetScreen = false;
+}
+*/
+
+
+//display button pressed
+const button = document.querySelectorAll("button");
+button.forEach(btn => {btn.addEventListener("click", function() {
+    appendNumber(this.value);
+})});
+
+//append num
+function appendNumber(num) {
+    if (curScreen === "0" || shouldResetScreen === true) resetScreen();
+    curScreen.innerText += num;
+}
+
+//append point
+function appendPoint() {
+    if (shouldResetScreen) resetScreen();
+    if (curScreen.innerText === "") curScreen.innerText = "0";
+    if (curScreen.innerText.includes(".")) return;
+    curScreen.innerText += ".";
+}
+
+//reset screen
+function resetScreen() {
+    curScreen = "0";
+    shouldResetScreen = false;
+}
+
+//clear screen
+function clr() {
+    opScreen.innerText = "";
+    curScreen.innerText = "";
+    first = "";
+    second = "";
+    operator = "";
+}
+
+//remove last character from string
+function del() {
+    curScreen.innerText = curScreen.innerText.slice(0, -1);
+}
+
+
+function add(a, b) {
     return a + b;
 }
 
-const subtract = function(a, b) {
+function subtract(a, b) {
     return a - b;
 }
 
-const multiply = function(a, b) {
+function multiply(a, b) {
     return a * b;
 }
 
-const divide = function(a, b) {
+function divide(a, b) {
     return a / b;
 }
 
-const first = "";
-const second = "";
-const operator = "";
-
-const operate = function(a, b, op) {
-    if (op === "+") {
-        add(a, b);
-    } else if (op === "-") {
-        subtract(a, b);
-    } else if (op === "*") {
-        multiply(a, b);
-    } else if (op === "/") {
-        divide(a, b);
+function operate(a, b, op) {
+    a = Number(a);
+    b = Number(b);
+    switch(op) {
+        case "+":
+            return add(a, b);
+        case "-":
+            return subtract(a, b);
+        case "÷":
+            return divide(a, b);
+        case "×":
+            return multiply(a, b);
     }
 }
-
-const button = document.querySelectorAll("button");
-button.forEach(btn => {btn.addEventListener("click", function() {
-    let btnValue = this.value;
-    document.getElementById("screen").innerText += btnValue;
-})});
